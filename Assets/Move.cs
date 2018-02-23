9f89bdc4b980aae36a21d4202eebe1bba3a7b52b
Kyu Wan Choi <rbdhks2002@gmail.com>
2018-02-23 14:12:04 +0900

    유니티 업로드

---
+++ Assets/Move.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float moveSpeed = 8f;
+    public ParticleSystem myFx;
+
+    CharacterController con;
+    Vector3 moveDirection = Vector3.zero;
+
+    float jumpSpeed = 8f;
+    float gravity = 30f;
+
+    private ParticleSystem buff;
+    private bool ctrlFlag = true;
+    //private float slideSpeed = 5f;
+    private Vector3 slidingV;
+
+
+    void Start () {
+        con = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(con.isGrounded) {
+            float h = Input.GetAxisRaw ( "Horizontal" );
+            float v = Input.GetAxisRaw ( "Vertical" );
+
+            moveDirection = ( new Vector3 ( h , 0f , v ) );
+            //transform.LookAt ( transform.position + moveDirection );
+
+            moveDirection = transform.TransformDirection ( moveDirection );
+            /*ㄴ로컬공간에서 월드공간으로 /direction/을 변환합니다.
+                이 연산은 transform의 스케일이나 위치에 영향 받지 않습니다. 반환된 벡터는 /direction/처럼 같은 길이를 갖습니다.
+             */
+            if(!ctrlFlag) {
+                moveDirection += slidingV;
+            }
+
+            moveDirection *= moveSpeed;
+            if ( Input.GetButtonDown ( "Jump" ) && ctrlFlag)
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        con.Move ( moveDirection * Time.deltaTime );
+
+        //if( buff != null) {
+        //    buff.transform.position = new Vector3 ( transform.position.x , transform.position.y-1f, transform.position.z );
+        //}
+	}
+    private void OnControllerColliderHit( ControllerColliderHit hit ) {
+
+        if( hit.collider.gameObject.name == "step3" ) {
+            
+            if( buff == null) { // if(!isHealing) { ~ }
+                //buff = Instantiate ( myFx , new Vector3 ( transform.position.x , transform.position.y-1f , transform.position.z ) , Quaternion.Euler(90f,0f,0f), transform );
+                buff = Instantiate ( myFx , transform.Find( "FxPos" ).transform.position , Quaternion.Euler ( 90f , 0f , 0f ) , transform );
+                Destroy ( buff.gameObject , 1.9f );
+                // Invoke("함수명" , 1.9f); -> 1.9초 뒤에 함수 호출
+            }
+
+        }
+        //print (hit.normal.z/ hit.normal.y); => 45도 => 1.0;
+        float degreeLimit = GetComponent<CharacterController> ().slopeLimit;
+        float curDegree = Vector3.Angle ( transform.up, hit.normal );
+        slidingV = new Vector3 ( hit.normal.x , hit.normal.y-5f, hit.normal.z );
+
+        //Debug.DrawRay ( transform.position , hit.normal , Color.magenta , 5f );
+
+        //print ("Move.cs >> " + curDegree );
+        if( curDegree > degreeLimit ) {
+            ctrlFlag = false;
+            moveDirection = slidingV;
+        } else {
+            ctrlFlag = true;
+        }
+    }
+
+
+}
---
+++ Assets/mouseLook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour {
+    public float sensitivity = 100f;
+    float rotationX;
+    float rotationY;
+
+    public GameObject shell;
+    GameObject character;
+
+    void Start () {
+        character = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update () {
+        float h = Input.GetAxis ( "Mouse X" );
+        float v = Input.GetAxis ( "Mouse Y" );
+        print ( "mouseLook.cs >> " + h );
+        rotationY += h * sensitivity * Time.deltaTime;
+        rotationX += v * sensitivity * Time.deltaTime;
+
+        if( rotationX > 85f ) rotationX = 85f;
+        if( rotationX < -70f ) rotationX = -70f;
+
+        transform.localRotation = Quaternion.AngleAxis ( -rotationX , Vector3.right );
+        character.transform.localRotation = Quaternion.AngleAxis ( rotationY , Vector3.up );
+
+        if ( Input.GetKeyDown ( "escape" ) ) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if ( Input.GetKey (KeyCode.Mouse0 ) ) {
+            var myShell = Instantiate ( shell , transform.Find ( "shellPos" ).transform.position , transform.rotation );
+            myShell.GetComponent<Rigidbody> ().AddForce ( transform.right * Random.Range(100f,300f));
+            //myShell.GetComponent<Rigidbody> ().AddTorque ( Random.insideUnitSphere * 100f );
+            //.AddTorque(Random.insideUnitySphere * force); (회전하는 힘)
+
+            Destroy ( myShell , 50.0f );
+        }
+	}
+}
